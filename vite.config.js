00045a1b35928e755864f6bee76720a31ce412ea
Claudia <sannaclaudia11@gmail.com>
2026-05-13 17:17:00 +0200

    deploy

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  base: '/CloudTrace_The_ephemeral_Evidence/', 
   plugins: [
     react(),
     tailwindcss(),
